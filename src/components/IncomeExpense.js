c07e31c17bb20e5e8d502b132c8286c80b02ce89
Ha Manh Do <hdevz.com@gmail.com>
2020-02-23 15:11:53 +0700

    Expense Tracker React Context API

---
+++ src/components/IncomeExpense.js
@@ -0,0 +1,23 @@
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState';
+
+export const IncomeExpense = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    const amounts = transactions.map(transaction => transaction.amount);
+
+    const income = amounts.filter(item => item > 0).reduce((acc, item) => item + acc, 0).toFixed(2)
+    const expense = amounts.filter(item => item < 0).reduce((acc, item) => item + acc, 0).toFixed(2)
+    return (
+        <div className="inc-exp-container">
+            <div>
+                <h4>Income</h4>
+                <p id="money-plus" className="money plus">+${Math.abs(income)}</p>
+            </div>
+            <div>
+                <h4>Expense</h4>
+                <p id="money-minus" className="money minus">-${Math.abs(expense)}</p>
+            </div>
+        </div>
+    )
+}
